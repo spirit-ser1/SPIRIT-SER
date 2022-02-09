d59b4df8d947e33ca2489330ee833ccfcac58faf
SPIRIT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2022-02-09 13:03:30 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -119,7 +119,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '917994093863@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./Spiritser/spirit.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Sed','Hi','spirit']
+const array = ['Sed','Hi','spirit','Bgm']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
